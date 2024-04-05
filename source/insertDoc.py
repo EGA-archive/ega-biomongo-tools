fc1e4ec5bca8be98fa570a5d5bce566791b1bc6d
m-huertasp <martahuertasp@gmail.com>
2024-04-05 10:25:17 +0200

    initialize scripts

---
+++ source/insertDoc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""insertDoc.py  :  Insert document in the desired MongoDB and collection """
+
+__author__ = "Marta Huertas"
+__version__ = "0.1"
+__maintainer__ = "Marta Huertas"
+__email__ = "marta.huertas@crg.eu"
+__status__ = "development"
+
+
+# Import Packages
+import mongoConnection
+from pymongo import MongoClient
+import json
+
+
+# 
---
+++ source/mongoConnection.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""mongo_connection.py  :  Credentials for MongoDB connection """
+
+__author__ = "Marta Huertas"
+__version__ = "0.1"
+__maintainer__ = "Marta Huertas"
+__email__ = "marta.huertas@crg.eu"
+__status__ = "development"
+
+# Connection details:
+mongo_host = 'localhost'  # Since you're using SSH tunneling
+mongo_port = 27018  # The local port you've forwarded to the remote MongoDB instance
+username = 'root'  # MongoDB root username
+password = 'example'  # MongoDB root password
+auth_source = 'admin'  # The authentication database
