57af48cac168be1b1f66a30c593060f1394d4694
Marta Huertas <marta.huertas@crg.eu>
2024-05-06 15:38:41 +0200

    add protection to avoid duplication in insertMany

--- source/insert.py
+++ source/insert.py
@@ -57,7 +57,7 @@ def insertOne(operation, db, collection_name, json_documents, name, method):
         # Merge the meta_info with the existing document
         collection.update_one({"_id": doc_id}, {"$set": {"meta_info": meta_info}})
 
-# Insert one function
+# Insert many function
 def insertMany(operation, db, collection_name, json_documents, name, method):
     """
     Insert one document in a specific collection from a database.
@@ -93,3 +93,54 @@ def insertMany(operation, db, collection_name, json_documents, name, method):
         ]
         # Merge the meta_info with the existing document
         collection.update_one({"_id": doc_id}, {"$set": {"meta_info": meta_info}})
+
+def insertMany(operation, db, collection_name, json_documents, name, method):
+    """
+    Insert one document in a specific collection from a database.
+    The collection will be created if it doesn't exist.
+    """
+    # Read the JSON file
+    with open(json_documents) as f:
+        documents = json.load(f)
+
+    # Access the collection:
+    collection = db[collection_name]
+
+    print(f"Inserting into {collection_name} collection")
+
+    # Get the unique identifier or combination of fields for each document
+    unique_identifiers = [doc['stable_id'] for doc in documents]
+
+    # Find existing documents with the same identifiers
+    existing_documents = collection.find({'stable_id': {'$in': unique_identifiers}})
+
+    # Get the list of existing identifiers
+    existing_identifiers = [doc['stable_id'] for doc in existing_documents]
+
+    # Filter out documents that are not already in the collection
+    new_documents = [doc for doc in documents if doc['stable_id'] not in existing_identifiers]
+
+    print(f"{len(existing_identifiers)} of your documents already exist in the {collection_name} collection")
+    # Insert only new documents into the collection
+    if new_documents:
+        result = collection.insert_many(new_documents)
+
+        # Print the number of inserted documents
+        print(f"Number of inserted documents: {len(result.inserted_ids)}")
+
+        print("Generating meta information about the process.")
+        # Get the ObjectId of the inserted process document
+        process_id = meta.insertMeta(db, name, method, operation, collection_name)
+
+        # Update each inserted document with a reference to the meta document and operation
+        for doc_id in result.inserted_ids:
+            meta_info = [
+                {
+                    "meta_id": str(process_id),
+                    "operation": operation
+                }
+            ]
+            # Merge the meta_info with the existing document
+            collection.update_one({"_id": doc_id}, {"$set": {"meta_info": meta_info}})
+    else:
+        print("No new documents to insert.")
