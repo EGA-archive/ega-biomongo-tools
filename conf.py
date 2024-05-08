3b94dcd7af0b83e80392d11d42ab3fc27a93d1c4
Marta Huertas <marta.huertas@crg.eu>
2024-05-08 10:39:12 +0200

    small update

--- conf.py
+++ conf.py
@@ -8,27 +8,27 @@
 operation='' # Operations: insert_one, insert_many, update_one, update_all, update_with_file, restore_one
 
 # Metadata information
-name='' # Name of the person that does this operation
-method='' # Method used to obtain or modify the data (e.g. Raw data EGAPRO)
+name='' # Name of the person that does this operation.
+method='' # Method used to obtain or modify the data (e.g. Raw data EGAPRO).
 
 # General information
 database_name='' # Name of the database.
-collection_name='' # Collection to be managed (analysis, dac, dataset, experiment, policy, run, sample, study)
+collection_name='' # Collection to be managed (analysis, dac, dataset, experiment, policy, run, sample, study).
 
 # Depending on the operation you should include the relevant information.
 
 # ---------
 # Insert needs:
 # ----------
-json_documents=f'path/to/json' # Path to the json documents to be included in the MongoDB
+json_documents=f'path/to/json' # Path to the json documents to be included in the MongoDB.
 
 # ----------
 # Update needs:
 # ----------
-update_field='' # Target field to be updated (no need if using a file)
-new_value='' # New value for the field (no need if using a file)
+update_field='' # Target field to be updated (no need if using a file).
+new_value='' # New value for the field (no need if using a file).
 update_criteria={'field_to_match':'value_to_match'} # Criteria to update one, pick a field with unique values.
-# If using update_with_file, please provide the csv with the information
+# If using update_with_file, please provide the csv with the information.
 update_file = 'path/to/csv'
 
 # ----------
