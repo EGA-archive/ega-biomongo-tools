6d25c565e9238286db7ed1a290d7ca40e8797875
Marta Huertas <marta.huertas@crg.eu>
2024-05-27 16:17:07 +0200

    implement bulk in insert_many

--- source/insert.py
+++ source/insert.py
@@ -11,6 +11,7 @@ __status__ = "development"
 
 # Import Packages
 import json
+from pymongo import UpdateOne
 from . import log_functions
 import os
 
@@ -62,60 +63,62 @@ def insertOne(operation, db, collection_name, json_documents, name, method):
     else:
         print(f'{json_documents} file does not exist')
 
-# Insert many function
 def insertMany(operation, db, collection_name, json_documents, name, method):
     """
-    Insert one document in a specific collection from a database.
+    Insert multiple documents into a specific collection from a database.
     The collection will be created if it doesn't exist.
     """
-    if os.path.exists(json_documents): # Test if json file exists
+    if os.path.exists(json_documents):  # Test if json file exists
         # Read the JSON file
         with open(json_documents) as f:
             documents = json.load(f)
 
-        # Access the collection:
+        # Access the collection
         collection = db[collection_name]
 
         print(f"Inserting into {collection_name} collection")
 
-        # Get the unique identifier or combination of fields for each document
+        # Get the unique identifier for each document
         unique_identifiers = [doc['stable_id'] for doc in documents]
 
         # Find existing documents with the same identifiers
         existing_documents = collection.find({'stable_id': {'$in': unique_identifiers}})
+        existing_identifiers = {doc['stable_id'] for doc in existing_documents}
 
-        # Get the list of existing identifiers
-        existing_identifiers = [doc['stable_id'] for doc in existing_documents]
-
-        # Filter out documents that are not already in the collection
+        # Filter out documents that are already in the collection
         new_documents = [doc for doc in documents if doc['stable_id'] not in existing_identifiers]
 
         print(f"{len(existing_identifiers)} of your documents already exist in the {collection_name} collection.")
+
         # Insert only new documents into the collection
         if new_documents:
             # Get the ObjectId of the inserted process document
             process_id = log_functions.insertLog(db, name, method, operation, collection_name)
 
             if process_id:
+                # Insert new documents
                 result = collection.insert_many(new_documents)
+                inserted_ids = result.inserted_ids
+
+                # Prepare bulk update operations to add the log field to the inserted documents
+                log_info = {
+                    "log_id": str(process_id),
+                    "operation": operation
+                }
+                bulk_updates = [
+                    UpdateOne({"_id": doc_id}, {"$set": {"log": [log_info]}})
+                    for doc_id in inserted_ids
+                ]
+
+                # Perform bulk update operations
+                if bulk_updates:
+                    collection.bulk_write(bulk_updates)
 
                 # Print the number of inserted documents
-                print(f"Number of inserted documents: {len(result.inserted_ids)}")
-
-
-                # Update each inserted document with a reference to the log document and operation
-                print("Generating log information about the process.")
-                for doc_id in result.inserted_ids:
-                    log = [
-                        {
-                            "log_id": str(process_id),
-                            "operation": operation
-                        }
-                    ]
-                    # Merge the log with the existing document
-                    collection.update_one({"_id": doc_id}, {"$set": {"log": log}})
+                print(f"Number of inserted documents: {len(inserted_ids)}")
+                print("Log information generated and added to the documents.")
             else:
-                print("Log details was not generated.")
+                print("Log details were not generated.")
         else:
             print("No new documents to insert.")
     else:
