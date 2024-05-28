6c1682664b6b53e9314b3072ee32217d8a89d69a
Marta Huertas <marta.huertas@crg.eu>
2024-05-28 14:58:09 +0200

    merge functions into one

--- source/insert.py
+++ source/insert.py
@@ -15,63 +15,21 @@ from pymongo import UpdateOne
 from . import log_functions
 import os
 
-# Insert one function
-def insertOne(operation, db, collection_name, json_documents, name, method):
+def insertDocuments(operation, db, collection_name, json_documents, name, method):
     """
-    Insert one document in a specific collection from a database.
+    Insert one or multiple documents into a specific collection from a database.
     The collection will be created if it doesn't exist.
     """
-    if os.path.exists(json_documents): # Test if json file exists
+    if os.path.exists(json_documents):  # Test if JSON file exists
         # Read the JSON file
         with open(json_documents) as f:
-            documents = json.load(f)
+            data = json.load(f)
 
-        # Access the collection:
-        collection = db[collection_name]
-        
-        print(f"Documents inserted into {collection_name} collection")
-
-        # Check if a document with the same ID already exists
-        existing_document = collection.find_one({'stable_id': documents["stable_id"]})
-
-        if existing_document:
-            print(f"A document with the same stable_id already exists in the {collection_name} collection.")
+        # Determine if data is a single document or a list of documents
+        if isinstance(data, list):
+            documents = data
         else:
-            # Insert the document into the collection
-            result = collection.insert_one(documents)
-
-            doc_id = result.inserted_id
-
-            # Print the inserted document ID
-            print("Inserted document ID:", result.inserted_id)
-
-            if result:
-                # Get the ObjectId of the inserted process document
-                process_id = log_functions.insertLog(db, name, method, operation, collection_name)
-
-                # Update inserted document with a reference to the log document and operation
-                log = [
-                    {
-                    "log_id" : str(process_id),
-                    "operation": operation
-                    }
-                ]
-                # Merge the log with the existing document
-                collection.update_one({"_id": doc_id}, {"$set": {"log": log}})
-            else:
-                print("Document could not be inserted.")
-    else:
-        print(f'{json_documents} file does not exist')
-
-def insertMany(operation, db, collection_name, json_documents, name, method):
-    """
-    Insert multiple documents into a specific collection from a database.
-    The collection will be created if it doesn't exist.
-    """
-    if os.path.exists(json_documents):  # Test if json file exists
-        # Read the JSON file
-        with open(json_documents) as f:
-            documents = json.load(f)
+            documents = [data]
 
         # Access the collection
         collection = db[collection_name]
@@ -88,18 +46,26 @@ def insertMany(operation, db, collection_name, json_documents, name, method):
         # Filter out documents that are already in the collection
         new_documents = [doc for doc in documents if doc['stable_id'] not in existing_identifiers]
 
-        print(f"{len(existing_identifiers)} of your documents already exist in the {collection_name} collection.")
+        if len(documents) == 1 and not existing_identifiers:
+            # Handle the single document insertion
+            document = documents[0]
+            result = collection.insert_one(document)
+            inserted_ids = [result.inserted_id]
+        else:
 
-        # Insert only new documents into the collection
-        if new_documents:
+            # Insert only new documents into the collection
+            if new_documents:
+                # Insert new documents
+                result = collection.insert_many(new_documents)
+                inserted_ids = result.inserted_ids
+            else:
+                inserted_ids = []
+
+        if inserted_ids:
             # Get the ObjectId of the inserted process document
             process_id = log_functions.insertLog(db, name, method, operation, collection_name)
 
             if process_id:
-                # Insert new documents
-                result = collection.insert_many(new_documents)
-                inserted_ids = result.inserted_ids
-
                 # Prepare bulk update operations to add the log field to the inserted documents
                 log_info = {
                     "log_id": str(process_id),
@@ -114,8 +80,8 @@ def insertMany(operation, db, collection_name, json_documents, name, method):
                 if bulk_updates:
                     collection.bulk_write(bulk_updates)
 
-                # Print the number of inserted documents
                 print(f"Number of inserted documents: {len(inserted_ids)}")
+                print(f"Number of documents already exising in the collection: {len(existing_identifiers)}")
                 print("Log information generated and added to the documents.")
             else:
                 print("Log details were not generated.")
